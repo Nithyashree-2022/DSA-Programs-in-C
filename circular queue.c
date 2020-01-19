5be1a7bc3630be28ceeabd5ede725a215759c4e2
Nithyashree-2022 <60055650+Nithyashree-2022@users.noreply.github.com>
2020-01-19 11:12:13 +0530

    Update circular queue.c

--- circular queue.c
+++ circular queue.c
@@ -1,95 +1,95 @@
- 
-
-			
-	
 # include <stdio.h>
 # define MAX  4
 int queue[MAX],front=0,rear=-1;
 
 void enqueue(int ele)
 {
-if((rear==MAX-1 && front==0) || (front-rear==1 && rear!=-1))
-printf("\nOverflow");
-else
-{
-rear=(rear+1)%MAX;
-queue[rear]=ele;
-printf("\nSuccessfully inserted:%d",ele);
+	if((rear==MAX-1 && front==0) || (front-rear==1 && rear!=-1))
+		printf("\nOverflow");
+	else
+	{
+		rear=(rear+1)%MAX;
+		queue[rear]=ele;
+		printf("\nSuccessfully inserted:%d",ele);
+	}
 }
-}
+
 void dequeue()
 {
-int i;
+	int i;
 
 
-if(rear==-1)
-{
-printf("\nUnderflow");
+	if(rear==-1)
+	{
+		printf("\nUnderflow");
+	}
+
+	else if(front!=rear)
+	{
+		printf("\ndeleted=%d",queue[front]);
+		front=(front+1)%MAX;
+	}
+	else
+	{
+		printf("\ndeleted=%d",queue[front]);
+		front=-1;
+		rear=-1;
+	}
 }
 
-else if(front!=rear)
-{
-printf("\ndeleted=%d",queue[front]);
-front=(front+1)%MAX;
-}
-else
-{
-printf("\ndeleted=%d",queue[front]);
-front=-1;
-rear=-1;
-}
-}
 void display()
 {
-int i=front;
-if(rear==-1)
-printf("\nEMPTY");
-else
-{
+	int i=front;
+	if(rear==-1)
+		printf("\nEMPTY");
+	else
+	{
 
-while(i!=rear)
-{
-printf("\t%d",queue[i]);
-i=(i+1)%MAX;
-}
-printf("\t%d",queue[i]);
-}
+		while(i!=rear)
+		{
+			printf("\t%d",queue[i]);
+			i=(i+1)%MAX;
+		}
+		printf("\t%d",queue[i]);
+	}
+
 }
 void main()
 {
-int ch,status=1,E;
-while(status==1)
-{
-printf("\nEnter ur choice-1.Enqueue,2.Dequeue,3.Display,4.EXIT");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1:
-{
-printf("\nEnter ele:");
-scanf("%d",&E);
-enqueue(E);
-break;
+	int ch,status=1,E;
+	while(status==1)
+	{
+		printf("\nEnter ur choice-1.Enqueue,2.Dequeue,3.Display,4.EXIT");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+			{
+				printf("\nEnter ele:");
+				scanf("%d",&E);
+				enqueue(E);
+				break;
+			}
+			case 2:
+			{
+				dequeue();
+				break;
+			}
+			case 3:
+			{
+				display();
+				break;
+			}
+			case 4:
+			{
+				status=0;
+				break;
+			}
+			default:
+			printf("\nInvalid choice");
+		}
+	}
 }
-case 2:
-{
-dequeue();
-break;
-}
-case 3:
-{
-display();
-break;
-}
-case 4:
-{
-status=0;
-break;
-}
-default:
-printf("\nInvalid choice");
-}
-}}
 
 
 	
